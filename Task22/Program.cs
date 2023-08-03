4cf8092ffcc8824753eb06a13daa283c23eeae9e
Svetlana Panina <paninasi@inbox.ru>
2023-08-03 09:57:52 +0300

    Task22

---
+++ Task22/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 22:
+// Напишите программу, которая
+// принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+// 5 –> 1, 4, 9, 16, 25.
+// 2 –> 1, 4.
+
+Console.Write("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    SquareTable(number);
+}
+else
+{
+    Console.WriteLine("Число не натуральное.");
+}
+
+void SquareTable(int num)
+{
+    int count = 1;
+    while (count <= number)
+    {
+        Console.WriteLine($"{count, 5}{count * count, 10}"); // Форматирование вывода.
+        count++;
+    }
+}
